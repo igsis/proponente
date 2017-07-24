e5fb28c27a583dc99cffed3236e58d393a496dac
Grayce Ferreira <grayce.ferreira@gmail.com>
2017-07-24 12:09:55 -0300

    VOLTAR - documentos e MENU

--- perfil/documentos_pf.php
+++ perfil/documentos_pf.php
@@ -193,7 +193,7 @@ $pf = recuperaDados("usuario_pf","id",$idPessoaFisica);
 				<!-- Botão para Voltar e Prosseguir -->
 				<div class="form-group">					
 					<div class="col-md-offset-2 col-md-2">
-						<form class="form-horizontal" role="form" action="?perfil=inicio" method="post">
+						<form class="form-horizontal" role="form" action="?perfil=inicio_pf" method="post">
 							<input type="submit" value="Voltar" class="btn btn-theme btn-lg btn-block"  value="<?php echo $idPessoaFisica ?>">
 						</form>	
 					</div>
