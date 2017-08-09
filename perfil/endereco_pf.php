0d47fc3d92012cb4d3982c8b51758edfff56e84c
LoreleiGab <loreleigab@hotmail.com>
2017-08-08 22:26:15 -0300

    Upload do documento Comprovante de Residência PF

--- perfil/endereco_pf.php
+++ perfil/endereco_pf.php
@@ -2,6 +2,8 @@
 $con = bancoMysqli();
 $idPessoaFisica = $_SESSION['idUsuario'];
 
+$idCampo = 3;
+$tipoPessoa = 1;
 
 if(isset($_POST['cadastrarEndereco']))
 {
@@ -24,6 +26,61 @@ if(isset($_POST['cadastrarEndereco']))
 	{
 		$mensagem = "Erro ao atualizar! Tente novamente.";
 	}	
+}
+
+if(isset($_POST["enviar"]))
+{
+	$sql_arquivos = "SELECT * FROM upload_lista_documento WHERE idTipoPessoa = '$tipoPessoa' AND id = '$idCampo'";
+	$query_arquivos = mysqli_query($con,$sql_arquivos);
+	while($arq = mysqli_fetch_array($query_arquivos))
+	{ 
+		$y = $arq['id'];
+		$x = $arq['sigla'];
+		$nome_arquivo = $_FILES['arquivo']['name'][$x];
+		
+		if($nome_arquivo != "")
+		{
+			$nome_temporario = $_FILES['arquivo']['tmp_name'][$x];		
+			$new_name = date("YmdHis")."_".semAcento($nome_arquivo); //Definindo um novo nome para o arquivo
+			$hoje = date("Y-m-d H:i:s");
+			$dir = '../uploadsdocs/'; //Diretório para uploads
+		
+			if(move_uploaded_file($nome_temporario, $dir.$new_name))
+			{  
+				$sql_insere_arquivo = "INSERT INTO `upload_arquivo` (`idTipoPessoa`, `idPessoa`, `idUploadListaDocumento`, `arquivo`, `dataEnvio`, `publicado`) VALUES ('$tipoPessoa', '$idPessoaFisica', '$idCampo', '$new_name', '$hoje', '1'); ";
+				$query = mysqli_query($con,$sql_insere_arquivo);
+			
+				if($query)
+				{
+					$mensagem = "Arquivo recebido com sucesso";
+				}
+				else
+				{
+					$mensagem = "Erro ao gravar no banco";
+				}
+				
+			}
+			else
+			{
+				 $mensagem = "Erro no upload"; 
+			}
+		}	
+	}
+}
+
+if(isset($_POST['apagar']))
+{
+	$idArquivo = $_POST['apagar'];
+	$sql_apagar_arquivo = "UPDATE upload_arquivo SET publicado = 0 WHERE id = '$idArquivo'";
+	if(mysqli_query($con,$sql_apagar_arquivo))
+	{
+		$mensagem =	"Arquivo apagado com sucesso!";
+		gravarLog($sql_apagar_arquivo);
+	}
+	else
+	{
+		$mensagem = "Erro ao apagar o arquivo. Tente novamente!";
+	}
 }
 
 $pf = recuperaDados("usuario_pf","id",$idPessoaFisica);
@@ -86,6 +143,55 @@ $pf = recuperaDados("usuario_pf","id",$idPessoaFisica);
 					</div>
 				</div>
 			</form>
+			
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8"><hr/></div>
+				</div>
+			
+				<!-- Exibir arquivos -->
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<div class="table-responsive list_info"><h6>Arquivo Anexado</h6>
+							<?php listaArquivoCampo($idPessoaFisica,$tipoPessoa,$idCampo,"endereco_pf"); ?>
+						</div>
+					</div>
+				</div>				
+				
+				
+				<!-- Upload de arquivo -->
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<div class = "center">
+						<form method="POST" action="?perfil=endereco_pf" enctype="multipart/form-data">
+							<table>
+								<tr>
+									<td width="50%"><td>
+								</tr>
+								<?php 
+									$sql_arquivos = "SELECT * FROM upload_lista_documento WHERE idTipoPessoa = '$tipoPessoa' AND id = '$idCampo'";
+									$query_arquivos = mysqli_query($con,$sql_arquivos);
+									while($arq = mysqli_fetch_array($query_arquivos))
+									{ 
+								?>
+										<tr>
+											<td><label><?php echo $arq['documento']?></label></td><td><input type='file' name='arquivo[<?php echo $arq['sigla']; ?>]'></td>
+										</tr>
+								<?php 
+									}
+								?>
+							</table><br>						
+							<input type="hidden" name="idPessoa" value="<?php echo $idPessoaFisica; ?>"  />
+							<input type="hidden" name="tipoPessoa" value="<?php echo $tipoPessoa; ?>"  />
+							<input type="submit" name="enviar" class="btn btn-theme btn-lg btn-block" value='Enviar'>
+						</form>
+						</div>
+					</div>
+				</div>
+				<!-- Fim Upload de arquivo -->
+				
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8"><hr/><br/></div>
+				</div>			
 		
 				<!-- Botão para Voltar e Prosseguir -->
 				<div class="form-group">					
