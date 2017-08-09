5623e35ccaf8d01ae17d837c49bbdf0dce61a8ca
William Cordeiro Fonseca <x355804@rede.sp>
2017-08-09 13:05:55 -0300

    mascaras

--- verifica_pf.php
+++ verifica_pf.php
@@ -6,6 +6,11 @@
 		<link href="visual/css/style.css" rel="stylesheet" media="screen">
 		<link href="visual/color/default.css" rel="stylesheet" media="screen">
 		<script src="visual/js/modernizr.custom.js"></script>
+		<script src="visual/js/jquery-1.9.1.js"></script>
+		<script src="visual/js/jquery.maskedinput.js" type="text/javascript"></script>
+		<script src="visual/js/jquery.maskMoney.js" type="text/javascript"></script>
+		<script type="text/javascript">
+	$(document).ready(function(){	$("#cpf").mask("999.999.999-99");});</script>
 	</head>
 	<body>	
 		<section id="services" class="home-section bg-white">
@@ -25,7 +30,7 @@
 						<div class="col-md-offset-2 col-md-8">
 							<form method="POST" action="login_resultado_pf.php" class="form-horizontal" role="form">
 								<label>Insira o CPF</label>
-								<input type="text" name="busca" class="form-control" placeholder="CPF" id="cpf" >
+								<input type="text" class="form-control" id="cpf" name="CPF"  placeholder="CPF">
 								<br />             
 								<div class="form-group">
 									<div class="col-md-offset-2 col-md-8">
