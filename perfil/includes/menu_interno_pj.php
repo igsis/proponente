fa19a623117aad3421dd5199349ee10d14be2092
LoreleiGab <loreleigab@hotmail.com>
2017-10-02 17:56:03 -0300

    Alterar o menu de PJ para que Dados Bancários fique após os itens de Representante para que lembrem-se se preencher os dados do representante antes de gerar FACC

--- perfil/includes/menu_interno_pj.php
+++ perfil/includes/menu_interno_pj.php
@@ -4,10 +4,10 @@
 			<strong>
 			| <a href="?perfil=inicio_pj">Informações Iniciais</a> 
 			| <a href="?perfil=documentos_pj">Documentos</a> 
-			| <a href="?perfil=endereco_pj">Endereço</a> 
-			| <a href="?perfil=dados_bancarios_pj">Dados Bancários</a>
+			| <a href="?perfil=endereco_pj">Endereço</a>
 			| <a href="?perfil=representante1_pj">Representante #1</a>
-			| <a href="?perfil=representante2_pj">Representante #2</a>
+			| <a href="?perfil=representante2_pj">Representante #2</a> 
+			| <a href="?perfil=dados_bancarios_pj">Dados Bancários</a>
 			| <a href="?perfil=anexos_pj">Demais Anexos</a>
 			| <a href="?perfil=senha_pj">Trocar a senha</a>
 			| <a href="../include/logoff.php">Sair</a> |</strong><br/>
