297072198444e3a8a735479e40557ef0dfb9f8f6
LoreleiGab <loreleigab@hotmail.com>
2017-10-09 18:26:27 -0300

    Ajustes de texto

--- index.php
+++ index.php
@@ -17,14 +17,16 @@
 					<div class="col-md-offset-1 col-md-10">
 						
 						<h2 align="justify">CADASTRO DE ARTISTAS E PROFISSIONAIS DE ARTE E CULTURA</h2>
-											
-						<p align="justify">O objetivo é credenciar os artistas, a fim de agilizar a contratação em eventos realizados pela Secretaria Municipal de Cultura de São Paulo.</p>
-
-						<p align="left">Uma vez cadastrados, esses artistas poderão atualizar suas informações e enviar a documentação necessária para o processo de contratação. Como o sistema possui ligação direta com o sistema da programação, a medida que o cadastro do artista no CAPAC encontra-se atualizado, o processo de contratação consequentemente é agilizado.</p>
-
-						<p align="left">Podem se cadastrar artistas ou grupos artísticos, como pessoa física ou jurídica.</p>
 						
-						<br />	
+						<br/>
+						
+						<p align="justify">Este sistema tem por objetivo criar um ambiente para credenciamento de artistas e profissionais de arte e cultura a fim de agilizar os processos de contratação artística em eventos realizados pela Secretaria Municipal de Cultura de São Paulo.</p>
+
+						<p align="justify">Uma vez cadastrados, esses artistas poderão atualizar suas informações e enviar a documentação necessária para o processo de contratação. Como o sistema possui ligação direta com o sistema da programação, a medida que o cadastro do artista no CAPAC encontra-se atualizado, o processo de contratação consequentemente é agilizado.</p>
+
+						<p align="justify">Podem se cadastrar artistas ou grupos artísticos, como pessoa física ou jurídica.</p>
+						
+						<br /><br />	
 						
 						<div class="form-group">
 							<div class="col-md-offset-1 col-md-3">
@@ -45,8 +47,13 @@
 		</section>   
 		<footer>
 			<div class="container">									
-				<p ><img src="visual/images/logo_cultura_q.png" align="left"/>
-				2017 @ IGSIS - Cadastro de Artistas e Profissionais de Arte e Cultura<br/>Secretaria Municipal de Cultura<br/>Prefeitura de São Paulo</p>
+				<table width="100%">
+					<tr>
+						<td><img src="visual/images/logo_cultura_q.png" align="left"/></td>
+						<td align="center"><font color="#ccc">2017 @ IGSIS - Cadastro de Artistas e Profissionais de Arte e Cultura<br/>Secretaria Municipal de Cultura<br/>Prefeitura de São Paulo</font></td>
+						<td><img src="visual/images/logo_igsis_azul.png" align="right"/></td>
+					</tr>
+				</table>
 			</div>	
 		</footer>
     </body>
