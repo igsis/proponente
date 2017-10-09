0dea43858a08f3a473197141c35cfc19ffe84000
LoreleiGab <loreleigab@hotmail.com>
2017-10-09 15:46:14 -0300

    Página inicial

--- index.php
+++ index.php
@@ -8,26 +8,23 @@
 		<script src="visual/js/modernizr.custom.js"></script>
 	</head>
 	<body>
+		<div id="bar">
+			<p id="p-bar">&nbsp;<!--<img src="images/logo_pequeno.png" />--></p>			
+		</div>
 		<section id="contact" class="home-section bg-white">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-offset-2 col-md-8">
-						<div class="text-hide">
-							<h4>Mapeamento e Cadastro de Artistas e Profissionais de Arte e Cultura</h4>
-						</div>
-					</div>
-				</div>
-				<div class="row">
 					<div class="col-md-offset-1 col-md-10">
-						<h6>O QUE É?</h6>
+						
+						<h2 align="justify">CADASTRO DE ARTISTAS E PROFISSIONAIS DE ARTE E CULTURA</h2>
+											
+						<p align="justify">O objetivo é credenciar os artistas para que eles possam trabalhar em eventos realizados pela Secretaria de Cultura.</p>
 
-						<p align="left">O objetivo é credenciar os artistas locais, para que eles possam trabalhar em eventos realizados ou apoiados pela Secretaria de Cultura, além de promover uma aproximação entre o poder público e os agentes culturais. Outra meta, que valoriza e fomenta a produção cultural local, é mapear os artistas e instituições artísticas presentes em Mogi das Cruzes.</p>
+						<p align="left">Uma vez cadastrados, esses artistas poderão ser convocados para projetos rotineiros da Secretaria Municipal de Cultura, bem como ações maiores, como a Virada Cultural, por exemplo. Nada impede, contudo, que em eventos de grande porte, chamamentos específicos sejam realizados.</p>
 
-						<p align="left">Uma vez cadastrados, esses artistas poderão ser convocados para projetos rotineiros da Secretaria Municipal de Cultura, bem como ações maiores, como Virada Cultural e o Festival de Inverno Serra do Itapety, por exemplo. Nada impede, contudo, que em eventos de grande porte, chamamentos específicos sejam realizados. Em 2014, um primeiro cadastramento já foi realizado e os artistas que se inscreveram na ocasião precisam agora atualizar e revalidar seus dados.</p>
+						<p align="left">Podem se cadastrar artistas ou grupos artísticos, como pessoa física ou jurídica, diretamente ou então por intermédio do empresário ou produtor. Empresários e produtores, aliás, também podem se cadastrar, bem como qualquer pessoa física ou jurídica que tenha experiência profissional em áreas relacionadas à cultura.</p>
 
-						<p align="left">Podem se cadastrar artistas ou grupos artísticos, como pessoa física ou jurídica, diretamente ou então por intermédio do empresário ou produtor. Empresários e produtores, aliás, também podem se cadastrar, bem como qualquer pessoa física ou jurídica que tenha experiência profissional em áreas relacionadas à cultura, tais como oficineiros, produtores de eventos, técnicos de gravação, técnicos de som, iluminotécnicos, monitores, curadores de exposição, jurados, etc.</p>
-
-						<p align="left">Uma vez no cadastro, todos os inscritos passarão por uma avaliação e terão uma posição específica dentro da listagem. Esta avaliação vai levar em conta critérios como originalidade, experiência comprovada, clareza, relevância, localidade e material enviado. O chamamento posterior desses artistas vai obedecer à classificação apontada pelo cadastro.</p>
+						<p align="left">...</p>
 						
 						<br />	
 						
@@ -48,6 +45,11 @@
 				</div>
 			</div>
 		</section>   
-		<?php include "visual/rodape.php" ?>
+		<footer>
+			<div class="container">									
+				<p ><img src="visual/images/logo_cultura_q.png" align="left"/>
+				2017 @ IGSIS - Cadastro de Artistas e Profissionais de Arte e Cultura<br/>Secretaria Municipal de Cultura<br/>Prefeitura de São Paulo</p>
+			</div>	
+		</footer>
     </body>
 </html>
