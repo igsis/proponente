aca720ea681023bd93a45c1d9880be681969184e
LoreleiGab <loreleigab@hotmail.com>
2017-10-11 10:19:37 -0300

    Ajustes no texto

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
 				<div class="row">
 					<div class="col-md-offset-1 col-md-10">
 						
-						<h2 align="justify">CADASTRO DE ARTISTAS E PROFISSIONAIS DE ARTE E CULTURA</h2>
+						<h2 align="justify">IGSIS - CADASTRO DE ARTISTAS E PROFISSIONAIS DE ARTE E CULTURA</h2>
 						
 						<br/>
 						
 						<p align="justify">Este sistema tem por objetivo criar um ambiente para credenciamento de artistas e profissionais de arte e cultura a fim de agilizar os processos de contratação artística em eventos realizados pela Secretaria Municipal de Cultura de São Paulo.</p>
 
-						<p align="justify">Uma vez cadastrados, esses artistas poderão atualizar suas informações e enviar a documentação necessária para o processo de contratação. Como o sistema possui ligação direta com o sistema da programação, a medida que o cadastro do artista no CAPAC encontra-se atualizado, o processo de contratação consequentemente é agilizado.</p>
+						<p align="justify">Uma vez cadastrados, esses artistas poderão atualizar suas informações e enviar a documentação necessária para o processo de contratação. Como o sistema possui ligação direta com o sistema da programação, a medida que o cadastro do artista no IGSIS - CAPAC encontra-se atualizado, o processo de contratação consequentemente é agilizado.</p>
 
 						<p align="justify">Podem se cadastrar artistas ou grupos artísticos, como pessoa física ou jurídica.</p>
 						
